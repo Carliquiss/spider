cd59bccadee61446da48f513b4c3a55d68895f2d
root <root@localhost.localdomain>
2019-07-12 11:26:24 +0200

    Segundo crawler añadido (el primero funciona más rápido)

--- spider.py
+++ spider.py
@@ -1,13 +1,16 @@
 import requests
 from parsel import Selector
-
 import time
+
+
+URL = 'http://dtstc.ugr.es/it/itt_st/'
+
 start = time.time()
 
 ### Crawling to the website
 
 # GET request to recurship site
-response = requests.get('http://recurship.com/')
+response = requests.get(URL)
 
 ## Setup for scrapping tool
 
@@ -17,21 +20,9 @@ selector = Selector(response.text)
 # Extracting href attribute from anchor tag <a href="*">
 href_links = selector.xpath('//a/@href').getall()
 
-
-#Extracting src attribute from img tag <img src="*">
-image_links = selector.xpath('//img/@src').getall()
-
-print('*****************************href_links************************************')
-print(href_links)
-print('*****************************/href_links************************************')
-
-
-
-print('*****************************image_links************************************')
-print(image_links)
-print('*****************************/image_links************************************')
-
+for enlaces in href_links:
+    print(enlaces)
 
 
 end = time.time()
-print("Time taken in seconds : ", (end-start))
+print("Ha tardado : ", (end-start))
